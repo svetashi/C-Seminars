76a7e751ae4ed449de4e1e3ed5680f61e9fd90fc
Sveta Shi <svetashiblieva@gmail.com>
2022-10-14 20:12:26 +0300

    добавила одну задачу №2

---
+++ HOMEWORK/Задача_2/Program.cs
@@ -0,0 +1,23 @@
+﻿//ДЗ (задача номер 2)
+
+Console.WriteLine("Введите число 1: ");
+string first = Console.ReadLine();
+Console.WriteLine("Введите число 2: ");
+string second = Console.ReadLine();
+
+int a = Convert.ToInt32(first);
+int b = Convert.ToInt32(second);
+
+if (a > b)
+{
+    Console.WriteLine($"Max : {a}");
+    Console.WriteLine($"Min : {b}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"Max : {b}");
+    Console.WriteLine($"Min : {a}");
+}
+else {
+    Console.WriteLine($"Число {a} равно {b}");
+}
