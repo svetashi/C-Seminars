d6119df249e9201f864fe5c22912686e7c61e562
Sveta Shi <svetashiblieva@gmail.com>
2022-11-15 20:51:00 +0300

    added homework 41,43

---
+++ HOMEWORK/Задача_43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void GetCross(double b1, double b2, double k1, double k2)
+{
+    double Xa = 0;
+    double Ya = 0;
+    Xa = (b2 - b1) / (k1 - k2);
+    Ya = (k2 * Xa) + b2;
+
+    Console.WriteLine($"Значенние пересечения первой координаты: {Xa}");
+    Console.WriteLine($"Значенние пересечения второй координаты: {Ya}");
+}
+
+
+Console.WriteLine("Введите первое значение для длины b :");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе значение для длины b :");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое значение для длины k :");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе значение для длины k :");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+GetCross(b1, b2, k1, k2);
--- HOMEWORK/черновик/Program.cs
+++ HOMEWORK/черновик/Program.cs
@@ -231,19 +231,121 @@
 // [1 2 3 4 5] -> 5 8 3
 // [6 7 3 6] -> 36 21
 
-//не доделана
+
+// int[] array1 = { 1, 2, 3, 4, 5 };
+// int[] array2 = new int[5];
+// int last = array1.Length - 1;
+
+// for (int i = 0; i <= last / 2; i++)
+// {
+//     array2[i] = array1[i] * array1[last - i];
+//     if (i == last / 2)
+//     {
+//         array2[i] = array1[i];
+//     }
+//     Console.Write($"{array2[i]}   ");
+// }
 
 
-int[] array1 = { 1, 2, 3, 4, 5 };
-int[] array2 = new int[5];
-int last = array1.Length - 1;
 
-for (int i = 0; i <= last / 2; i++)
+//Лекция по рекурсиям
+
+// string[,] table = new string [2,5];
+// String.Empty
+//table[0,0] table [0,1]...
+//table[1,0] [1,1]
+
+// table[1,2]= "name";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int colums = 0; colums < 5; colums++)
+//     {
+//         Console.WriteLine($"-{table[rows,colums]}-");
+//     }
+// }
+
+//пример заполенения таблицы
+
+// void PrintArray(int[,] matr)
+// {
+
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             Console.Write($"{matr[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// void FillArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i,j] = new Random().Next(1,10);
+//         }
+//     }
+// }
+
+// int [,] matrix = new int [3,4];
+// PrintArray(matrix);
+// FillArray(matrix);
+// Console.WriteLine();
+// PrintArray(matrix);
+
+
+//Факториал и рекурсия
+
+// double Fact(int n)
+// {
+//     if (n ==1) return 1;
+//     else return n * Fact(n-1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Fact(i)}");
+// }
+
+
+// FIBONACHI NUMBERS
+
+// f(1) = 1
+// f(2)= 1
+// f(n)= f(n-1)+f(n-2)
+
+// double Fib(int n)
+// {
+//     if (n==1 || n==2) return 1;
+//     else return Fib(n-1) + Fib(n-2);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"f({i}) = {Fib(i)}");
+// }
+
+
+// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.WriteLine("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int [] fibo= new int [value];
+
+fibo[0] =0;
+fibo[1] =1;
+Console.Write($"{fibo[0]} ");
+Console.Write($"{fibo[1]} ");
+
+for (int i = 2; i < fibo.Length; i++)
 {
-    array2[i] = array1[i] * array1[last - i];
-    if (i == last / 2)
-    {
-        array2[i] = array1[i];
-    }
-    Console.Write($"{array2[i]}   ");
+    fibo[i] = fibo[i-1] + fibo[i-2];
+    Console.Write($"{fibo[i]} ");
 }
+ 
+
